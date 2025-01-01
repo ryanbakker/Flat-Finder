39602204c40ee71b775eb1caf8b2e3ffce4bd8f5
Ryan Bakker <112455939+ryanbakker@users.noreply.github.com>
2025-01-02 12:03:38 +1300

    Implement Tabs Navigation

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,13 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-
-      <Text className="font-bold text-3xl font-rubik my-10">Welcome to FlatFinder</Text>
-
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
+      <Text className="font-bold text-3xl font-rubik my-10">
+        Welcome to FlatFinder
+      </Text>
     </View>
   );
 }
